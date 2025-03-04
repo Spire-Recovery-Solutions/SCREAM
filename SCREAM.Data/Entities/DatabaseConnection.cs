b3904d6e1253ff5aa020f012d0d5fc0eb138a6c1
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-03-04 13:27:08 -0500

    Added DatabaseConnection

---
+++ SCREAM.Data/Entities/DatabaseConnection.cs
@@ -0,0 +1,22 @@
+using SCREAM.Web.Enums;
+
+namespace SCREAM.Data.Entties
+{
+   public class DatabaseConnection
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string HostName { get; set; } = string.Empty;
+        public required int Port { get; set; } = 3306;
+        public required string UserName { get; set; } = string.Empty;
+        public required string Password { get; set; } = string.Empty;
+        public required DatabaseType Type { get; set; }
+
+        public string ConnectionString 
+        { 
+            get 
+            {
+                return $"Server={HostName};Port={Port};User ID={UserName};Password={Password};";
+            } 
+        }
+    }
+}
--- SCREAM.Data/ScreamDbContext.cs
+++ SCREAM.Data/ScreamDbContext.cs
@@ -1,12 +1,22 @@
 using Microsoft.EntityFrameworkCore;
+using SCREAM.Data.Entties;
+using SCREAM.Models.Database;
 
 namespace SCREAM.Data;
 
 public class ScreamDbContext(DbContextOptions<ScreamDbContext> options) : DbContext(options)
 {
+    public DbSet<DatabaseConnection> DatabaseConnections { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+      modelBuilder.Entity<DatabaseConnection>(entity =>
+            {
+                entity.ToTable("DatabaseConnections");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Type)
+                      .HasConversion<string>();
+            });   
         base.OnModelCreating(modelBuilder);
     }
 }
