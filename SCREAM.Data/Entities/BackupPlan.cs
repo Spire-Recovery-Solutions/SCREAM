1cfd62ca22d452aa34cfa42b6a1ff3fb79736761
Adam Reed <adam@spirerecoverysolutions.com>
2025-03-11 13:41:10 -0500

    feat: refactor BackupPlan and BackupSchedule for improved scheduling and job management

--- SCREAM.Data/Entities/BackupPlan.cs
+++ SCREAM.Data/Entities/BackupPlan.cs
@@ -1,5 +1,6 @@
 using SCREAM.Data.Entities.BackupItems;
 using SCREAM.Data.Entities.StorageTargets;
+using SCREAM.Data.Enums;
 
 namespace SCREAM.Data.Entities;
 
@@ -9,8 +10,33 @@ public class BackupPlan : ScreamDbBaseEntity
     public required string Description { get; set; }
     public required DatabaseConnection DatabaseConnection { get; set; }
     public required StorageTarget StorageTarget { get; set; }
-    public required BackupSchedule Schedule { get; set; }
     public required List<BackupItem> Items { get; set; }
-    
     public bool IsActive { get; set; }
+    
+    public ICollection<BackupJob> Jobs { get; set; }
+    
+    public string ScheduleCron { get; set; } = string.Empty;
+
+    public ScheduleType ScheduleType { get; set; }
+
+    public DateTime? LastRun { get; set; }
+
+    public DateTime? NextRun { get; set; }
+
+    public DateTime? GetNextRun(DateTime utcNow)
+    {
+        switch (ScheduleType)
+        {
+            case ScheduleType.Repeating:
+            {
+                var expression = Cronos.CronExpression.Parse(ScheduleCron);
+                var nextUtc = expression.GetNextOccurrence(DateTime.UtcNow);
+                return nextUtc;
+            }
+            case ScheduleType.OneTime when LastRun == null:
+                return CreatedAt.AddMinutes(5);
+            default:
+                return null;
+        }
+    }
 }
--- SCREAM.Data/Entities/BackupSchedule.cs
+++ SCREAM.Data/Entities/BackupSchedule.cs
@@ -1,9 +1,13 @@
-﻿using SCREAM.Data.Enums;
+﻿using Cronos;
+using SCREAM.Data.Enums;
 
 namespace SCREAM.Data.Entities
 {
     public class BackupSchedule : ScreamDbBaseEntity
-    {
+    {   
+        public long BackupPlanId { get; set; }
+        public BackupPlan BackupPlan { get; set; } = null!;
+        
         public string CronExpression { get; set; } = string.Empty;
 
         public ScheduleType ScheduledType { get; set; }
@@ -11,7 +15,22 @@ namespace SCREAM.Data.Entities
         public DateTime? LastRun { get; set; }
 
         public DateTime? NextRun { get; set; }
-        public BackupPlan BackupPlan { get; set; } = null!;
-        public long BackupPlanId { get; set; }
+
+        public DateTime? GetNextRun(DateTime utcNow)
+        {
+            switch (ScheduledType)
+            {
+                case ScheduleType.Repeating:
+                {
+                    var expression = Cronos.CronExpression.Parse(CronExpression);
+                    var nextUtc = expression.GetNextOccurrence(DateTime.UtcNow);
+                    return nextUtc;
+                }
+                case ScheduleType.OneTime when LastRun == null:
+                    return CreatedAt.AddMinutes(5);
+                default:
+                    return null;
+            }
+        }
     }
 }
