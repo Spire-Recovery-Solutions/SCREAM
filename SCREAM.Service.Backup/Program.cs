6d1c6adfb42c106ed9043fef79ed6aeb32579920
Vimal <vimal.kumar@spirerecoverysolutions.com>
2025-04-02 11:02:33 -0400

    Fix for backup

--- SCREAM.Service.Backup/Program.cs
+++ SCREAM.Service.Backup/Program.cs
@@ -5,15 +5,17 @@ namespace SCREAM.Service.Backup;
 
 public class Program
 {
-
     public static void Main(string[] args)
     {
         var builder = Host.CreateApplicationBuilder(args);
-         builder.Services.AddHttpClient("SCREAM", client =>
-            {
-                client.BaseAddress = new Uri("http://localhost:5102");
-                client.DefaultRequestHeaders.Add("Accept", "application/json");
-            });
+
+        var apiUrl = Environment.GetEnvironmentVariable("API_URL") ?? "http://localhost:5102";
+
+        builder.Services.AddHttpClient("SCREAM", client =>
+        {
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+        });
 
         builder.Services.AddHostedService<Worker>();
         var host = builder.Build();
